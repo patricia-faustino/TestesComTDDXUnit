9ea409445892641aba9bbf3b1cb8438d3c1ed7e5
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-08-02 22:28:48 -0300

    avaliando leilão

---
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 1200);
+            leilao.RecebeLance(fulano, 990);
+
+            leilao.TerminaPregao();
+
+            Console.WriteLine(leilao.Ganhador.Valor);
+        }
+    }
+}
